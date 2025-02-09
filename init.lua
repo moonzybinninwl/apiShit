714dcef283810632a6005202d5083576dd68a23a
moonzybinninwl <moonzyexploits@gmail.com>
2025-02-09 19:50:17 +1100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- open source because lol 
 print("Env by binninwl, salad and bery")
-print("Stellar: ENV version 1.2.1.2")
+print("Stellar: ENV version 1.2.1.5")
 getgenv().IS_STELLAR_LOADED = false
 local oldr = request 
 getgenv().request = function(options)
